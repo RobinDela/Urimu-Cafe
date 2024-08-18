afe0d4e8c7c25028952c2668d1480793750b5e64
RobinDela <78260403+RobinDela@users.noreply.github.com>
2024-08-18 16:45:16 +0900

    Size

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -11,8 +11,8 @@ const FindUs = () => (
       <div className="app__wrapper-content">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2891.7864998322825!2d144.42731527617119!3d43.54849217110729!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5f6d791c1c40a9fd%3A0x6472db200775f5ac!2z44Kr44OV44Kn44GG44KK44KA44GF!5e0!3m2!1sfr!2sjp!4v1723962047221!5m2!1sfr!2sjp"
-          width="600"
-          height="450"
+          width="50%"
+          height="45%"
           style={{ border: 0 }}
           allowFullScreen
           loading="lazy"
